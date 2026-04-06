cb1a28a9ba537dae8dbc866ba0a0480af59c207b
fairus0305 <ruszezzy@gmail.com>
2026-04-06 15:29:54 +0700

    Menambahkan input data struct Orang

--- StuructNew.cpp
+++ StuructNew.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
 using namespace std;
 
-struct orang
+struct Orang
 {
     string nama;
     string alamat;
     int umur;
 };
 
+int main(){
+    Orang mhs;
+    cout << "Nama = ";
+    cin >> mhs.nama;
+    cout << "Alamat = ";
+    cin >> mhs.alamat;
+    cout << "Umur = ";
+    cin >> mhs.umur;
+    cout << endl;
+}
